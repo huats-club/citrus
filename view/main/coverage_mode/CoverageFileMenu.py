32d9ad3e74b1ad869a237633529ec2e6a1f3b0e0
huiminlim <e0036379@u.nus.edu>
2021-09-12 21:45:22 +0800

    add dxf file load

--- view/main/coverage_mode/CoverageFileMenu.py
+++ view/main/coverage_mode/CoverageFileMenu.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import filedialog as tkfd
 from tkinter import ttk
 
 
@@ -24,8 +25,49 @@ class CoverageFileMenu(ttk.LabelFrame):
         self.container = ttk.Frame(self)
         self.container.pack(
             padx=10,
-            pady=(10, 5),
+            pady=10,
             side=tk.TOP,
             anchor=tk.NW,
             fill=tk.BOTH
         )
+
+        # Create option for file to open
+        self.selected_dxf = tk.StringVar()
+
+        # Container to store filepath choosing part
+        self.filepath_container = tk.Frame(
+            self.container
+        )
+        self.filepath_container.pack()
+
+        # Filepath entry display
+        self.filepath_text = tk.StringVar()
+        self.filepath_entry = ttk.Entry(
+            self.filepath_container,
+            width=30,
+            textvariable=self.filepath_text,
+            state="readonly"
+        )
+        self.filepath_entry.pack(
+            side=tk.LEFT
+        )
+        # Button to invoke dialog to select file to open
+        self.filepath_entry_button = ttk.Button(
+            self.filepath_container,
+            style="primary.Outline.TButton",
+            text="Select file",
+            state="normal",
+            command=self.get_dxf_filepath
+        )
+        self.filepath_entry_button.pack(
+            side=tk.RIGHT
+        )
+
+    def get_dxf_filepath(self):
+        try:
+            self.path = tkfd.askopenfilename(
+                initialdir="C:/", filetypes=(("dxf files", "*.dxf"),
+                                             ("all files", "*.*")))
+            self.filepath_text.set(self.path)
+        except ValueError:
+            self.filepath_text.set("")
