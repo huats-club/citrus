60de2e22e12273fad892ba590f703c18949d145d
huiminlim <e0036379@u.nus.edu>
2021-09-12 15:46:56 +0800

    add wifi exploration progress

--- exploration/wifi_test.py
+++ exploration/wifi_test.py
@@ -1,6 +1,7 @@
 from scapy.config import conf
 from scapy.interfaces import IFACES
 from scapy.layers.dot11 import Dot11
+from scapy.layers.inet6 import IPv6
 from scapy.sendrecv import sniff
 
 IFACE_NAME = "Realtek 8821AE Wireless LAN 802.11ac PCI-E NIC"
@@ -8,11 +9,17 @@ devices = set()
 
 
 def packethandler(pkt):
-    if pkt.haslayer(Dot11):
-        dot11_layer = pkt.getlayer(Dot11)
-        if dot11_layer.addr2 and (dot11_layer.addr2 not in devices):
-            devices.add(dot11_layer.addr2)
-            print(dot11_layer.addr2)
+    # if pkt.haslayer(Dot11):
+    #     dot11_layer = pkt.getlayer(Dot11)
+    #     if dot11_layer.addr2 and (dot11_layer.addr2 not in devices):
+    #         devices.add(dot11_layer.addr2)
+    #         print(dot11_layer.addr2)
+
+    # print(pkt.summary())
+    # print(pkt.show())
+    # print(pkt.layers())
+    if pkt.haslayer(IPv6):
+        print("scapy.layers.inet6.IPv6")
 
 
 conf.sniff_promisc = False
