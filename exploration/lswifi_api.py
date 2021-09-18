eb2fb2520ffa52153e4b09bb9fa90321b0fa3e0c
huiminlim <e0036379@u.nus.edu>
2021-09-18 16:46:03 +0800

    add basic scan ssid

--- exploration/lswifi_api.py
+++ exploration/lswifi_api.py
@@ -1,4 +1,10 @@
-from lswifi import core
+from lswifi import appsetup
+from lswifi.client import Client, get_interface_info
 from lswifi.wlanapi import WLAN
 
-core.list_interfaces(interfaces=WLAN.get_wireless_interfaces())
+# core.list_interfaces(interfaces=WLAN.get_wireless_interfaces())
+parser = appsetup.setup_parser()
+args = parser.parse_args()
+interfaces = WLAN.get_wireless_interfaces()
+for interface in interfaces:
+    print(get_interface_info(args, interface))
