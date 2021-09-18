ec736a7e5c2f9370cd7e2faac73f000f38b016b8
huiminlim <e0036379@u.nus.edu>
2021-09-18 16:53:26 +0800

    stop print interface

--- exploration/lswifi_api.py
+++ exploration/lswifi_api.py
@@ -8,7 +8,8 @@ parser = appsetup.setup_parser()
 args = parser.parse_args()
 interfaces = WLAN.get_wireless_interfaces()
 for interface in interfaces:
-    print(get_interface_info(args, interface))
+    # print(get_interface_info(args, interface))
+    pass
 
 client = Client(args, interface)
 data = client.get_bss_list(interface=interface)
