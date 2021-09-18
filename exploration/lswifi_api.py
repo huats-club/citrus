4c5d9c0c4bf619d0518ae5e968f4a4c690ccdb01
huiminlim <e0036379@u.nus.edu>
2021-09-18 16:52:18 +0800

    extract list of wifi ssid and strength

--- exploration/lswifi_api.py
+++ exploration/lswifi_api.py
@@ -1,5 +1,6 @@
 from lswifi import appsetup
 from lswifi.client import Client, get_interface_info
+from lswifi.core import parse_bss_list_and_print
 from lswifi.wlanapi import WLAN
 
 # core.list_interfaces(interfaces=WLAN.get_wireless_interfaces())
@@ -8,3 +9,7 @@ args = parser.parse_args()
 interfaces = WLAN.get_wireless_interfaces()
 for interface in interfaces:
     print(get_interface_info(args, interface))
+
+client = Client(args, interface)
+data = client.get_bss_list(interface=interface)
+parse_bss_list_and_print(data, args)
