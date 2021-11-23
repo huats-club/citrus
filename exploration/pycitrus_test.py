76dc3acd03d3f179467285a64a70d99c0ef5c971
huiminlim <e0036379@u.nus.edu>
2021-11-23 16:13:46 +0800

    change output file name

--- exploration/pycitrus_test.py
+++ exploration/pycitrus_test.py
@@ -6,6 +6,6 @@ p = pycitrus.CitrusProcessor(740.3e6)
 out = p.run()
 
 name = "{:%Y-%m-%d-%H-%M-%S}".format(datetime.datetime.now())
-with open(f'pycitrus_data/output_{name}_1.txt', "w") as f:
+with open(f'pycitrus_data/output_{name}.txt', "w") as f:
     for item in out:
         f.write(str(item) + '\n')
