9deee1f111f970d4bf5c1f64fce2214de07e0e7d
huiminlim <e0036379@u.nus.edu>
2021-11-29 21:09:10 +0800

    update waterfall plot

--- exploration/waterfall.py
+++ exploration/waterfall.py
@@ -5,48 +5,19 @@ import math
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from matplotlib import cm  # colour map
-from scipy import signal  # spectrogram function
-from scipy.interpolate import griddata
 
 
-def sample_plot():
-    # basic config
-    sample_rate = 11240.  #
-    sig_len_secs = 10
-    frequency = 2000.
+def gen_data():
+    '''
+    Generate and save datas to txt file once
+    '''
 
-    # generate the signal
-    timestamps_secs = np.arange(sample_rate*sig_len_secs) / sample_rate
-    mysignal = np.sin(2.0 * np.pi * frequency * timestamps_secs)
-
-    # extract the spectrum
-    freq_bins, timestamps, spec = signal.spectrogram(mysignal, sample_rate)
-    print(type(freq_bins))
-    print(type(timestamps))
-    print(type(spec))
-    print(freq_bins.ndim)
-    print(timestamps.ndim)
-    print(spec.ndim)
-
-    # 3d plot
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(freq_bins[:, None], timestamps[None, :], 10.0*np.log10(spec), cmap=cm.get_cmap("coolwarm"))
-    ax.set_xlabel('freq_bins')
-    ax.set_ylabel('timestamps')
-    ax.set_zlabel('spec')
-    plt.show()
-
-
-if __name__ == "__main__":
     # create timestamp data
     sample_rate = 11240
     sig_len_secs = 1
     timestamps_secs = np.arange(sample_rate*sig_len_secs) / sample_rate
     ts_np = np.asarray(timestamps_secs)
-    print(ts_np.shape)
 
     # generate frequency bins data
     freq_increment = 20e6 / 2048
@@ -57,7 +28,6 @@ if __name__ == "__main__":
         freq_bins.append(freq)
         freq += freq_increment
     freq_bins_np = np.asarray(freq_bins)
-    print(freq_bins_np.shape)
 
     # read in fft data
     fft_data = []
@@ -67,8 +37,9 @@ if __name__ == "__main__":
 
     # process fft data to dbm
     signal_data = [20*math.log10(2*x / 1024) for x in fft_data]
-
-    X, Y = np.meshgrid(freq_bins_np, ts_np)
+    freq_bins_np_X, ts_np_Y = np.meshgrid(freq_bins_np, ts_np)
+    np.save("C:/Users/65844/Desktop/citrus/exploration/3dplot_data/freq_bins_np", freq_bins_np_X)
+    np.save("C:/Users/65844/Desktop/citrus/exploration/3dplot_data/ts_np", ts_np_Y)
 
     # create z axis 2d data
     # format: f(x, y) -> z
@@ -76,20 +47,38 @@ if __name__ == "__main__":
     for i in range(ts_np.shape[0]):
         dataset.append(signal_data)
     dataset_np = np.asarray(dataset)
+    np.save("C:/Users/65844/Desktop/citrus/exploration/3dplot_data/dataset_np", dataset_np)
+
+
+if __name__ == "__main__":
+    root = 'C:/Users/65844/Desktop/citrus/exploration/3dplot_data/'
+    ts_np = np.load(f"{root}/ts_np.npy")
+    freq_bins_np = np.load(f"{root}/freq_bins_np.npy")
+    dataset_np = np.load(f"{root}/dataset_np.npy")
+    print(ts_np.shape)
+    print(freq_bins_np.shape)
     print(dataset_np.shape)
 
     # 3d plot
-    fig = plt.figure()
+    fig = plt.figure(1)
     ax = fig.add_subplot(projection='3d')
     surf = ax.plot_surface(
-        X,
-        Y,
+        freq_bins_np,
+        ts_np,
         dataset_np,
-        cmap=cm.get_cmap("coolwarm"),
+        cmap=cm.get_cmap("rainbow"),
         antialiased=True
     )
-    fig.colorbar(surf, shrink=0.5)
+    fig.colorbar(
+        surf,
+        location="bottom",
+        orientation="horizontal",
+        ax=ax,
+        shrink=0.4,
+        aspect=30
+    )
     ax.set_xlabel('freq_bins')
     ax.set_ylabel('timestamps')
     ax.set_zlabel('spec')
+    fig.savefig(f"{root}/3d.png")
     plt.show()
