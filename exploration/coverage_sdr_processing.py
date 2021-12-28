255a6ee9d4670c391dc4455fcd043d4ffa60a685
huiminlim <e0036379@u.nus.edu>
2021-12-28 22:34:58 +0800

    add search for max in region

--- exploration/coverage_sdr_processing.py
+++ exploration/coverage_sdr_processing.py
@@ -1,4 +1,4 @@
-import copy
+import math
 
 import numpy as np
 import pycitrus
@@ -38,16 +38,32 @@ if __name__ == "__main__":
     # get stats to locate the spikes
     avg = np.average(data)
     count = np.size(data)
-    print(avg, count)
-
-    threshold = -50
+    print(f"avg value: {avg:.5f}, count: {count}")
 
     # start, end, freqbin(increment) per idx
     freq_inc = 10e6 / count
-    print(f"freq increment: {freq_inc}")
+    print(f"freq increment: {freq_inc:.5f}")
 
-    # get each number and compare wrt threshold
-    # identify a cluster spike when the indexes are "close" to each other
-    for i in range(count):
-        if data[i] > threshold:
-            print(i, data[i])
+    # # get each number and compare wrt threshold
+    # # identify a cluster spike when the indexes are "close" to each other
+    # for i in range(count):
+    #     if data[i] > threshold:
+    #         print(i, data[i])
+
+    center_freq = 743.3e6
+    bandwidth = 10e6
+    start_freq = center_freq - 0.5*bandwidth
+    end_freq = center_freq + 0.5*bandwidth
+    print(f"start freq: {start_freq}, center_freq: {center_freq}, end_freq: {end_freq}")
+
+    # compute idx nearest to this center freq
+    center_idx = math.ceil((center_freq - start_freq) / freq_inc)
+    left_bound_idx = center_idx - 3
+    right_bound_idx = center_idx + 3
+    print(f"searching: {left_bound_idx} -> {right_bound_idx}")
+
+    max = -1000
+    for i in range(left_bound_idx, right_bound_idx+1):
+        if data[i] > max:
+            max = data[i]
+    print(f"found max: {max:.5f}")
