055c7f73ab661f0d7d40c63e1cefc7f8c6477885
huiminlim <e0036379@u.nus.edu>
2022-01-08 14:51:44 +0800

    test yaml write

--- exploration/yaml_test.py
+++ exploration/yaml_test.py
@@ -8,3 +8,9 @@ if __name__ == '__main__':
     with open(os.getcwd() + "/exploration/yaml_data/foo.yaml", 'r') as f:
         data = yaml.load(f, Loader=yaml.SafeLoader)
         print(data)
+
+    # Test yaml dumping
+    users = [{'name': 'John Doe', 'occupation': 'gardener'},
+             {'name': 'Lucy Black', 'occupation': 'teacher'}]
+    with open(os.getcwd() + "/exploration/yaml_data/write.yaml", 'w') as f:
+        data = yaml.dump(users, f)
