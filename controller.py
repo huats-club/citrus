bb70f677f91d94107537469d1753a4ac399ca1fa
huiminlim <e0036379@u.nus.edu>
2022-05-07 21:42:55 +0800

    add comments

--- controller.py
+++ controller.py
@@ -15,15 +15,18 @@ class Controller:
         # Create start page
         self.start = StartPage(self.container, self)
 
+    # Method is invoked when the user clicks on "start" in Start page
     def on_start_button_press(self):
 
+        # Get project settings from the Start page
         type_session, filepath = self.start.get_project_settings()
 
-        # Valid user input
+        # Valid user input from project settings
         is_valid_project_setting = (
             (filepath != "") and type_session == app_parameters.PROJECT_LOAD) or (
             type_session == app_parameters.PROJECT_NEW)
 
+        # If valid project settings, then proceed to Main page
         if is_valid_project_setting:
 
             # Wipe out current window
@@ -32,11 +35,13 @@ class Controller:
             # Switch over to main page
             self.main_page = MainPage(self.view, self)  # parent of main page is root
 
+        # If invalid project settings, then indicate error message
         else:
             # invalid
             self.start.display_error_message()
             return
 
+    # Method is invoked when the app exits
     def on_exit(self):
 
         # Destroy entire window to exit
